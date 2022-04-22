245310c7fc0886dd8d0fbb24659b01889c57cd30
Mariana <mariana.hb95@gmail.com>
2022-04-22 15:09:29 -0700

    Contains modularized files and json file configurartion to run with node index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = () => {
-  // ...
-};
+const process = require('process');
+
+const { validPath, absolutePath } = require('./lib/mdlinks');
+
+const mdLinks = (verifyCallback) => verifyCallback();
+
+if (process.argv[2] === '--valid') {
+  console.log(mdLinks(validPath));
+} else if (process.argv[2] === '--absolute') {
+  console.log(mdLinks(absolutePath));
+} else {
+  console.log('Not valid');
+}
+// module.exports = () => {
+//   // ...
+// };
