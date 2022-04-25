5ad4be452057836f11cbe7501ff3c14f00c24b13
Mariana <mariana.hb95@gmail.com>
2022-04-25 15:51:48 -0700

    verifies if given input is a valid path, resolves path if relative and sorts if file or directory

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const process = require('process');
 
 const { validPath, absolutePath } = require('./lib/mdlinks');
+const { verifyInput } = require('./lib/resolvePath');
 
+const userInput = process.argv[3];
 const mdLinks = (verifyCallback) => verifyCallback();
 
 if (process.argv[2] === '--valid') {
   console.log(mdLinks(validPath));
 } else if (process.argv[2] === '--absolute') {
   console.log(mdLinks(absolutePath));
+} else if (process.argv[2] === '--resolve') {
+  console.log(verifyInput(userInput), 'mdlinks');
 } else {
   console.log('Not valid');
 }
